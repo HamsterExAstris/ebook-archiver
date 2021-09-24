8312f1c92e9e661f314ed3571061cad234dc7239
Andrew Timson <6879630+HamsterExAstris@users.noreply.github.com>
2021-09-24 13:57:29 -0400

    Upgrade to Bootstrap 5.
    Includes bundling change to Gulp since BuildBundlerMinifier is no longer supported and is incompatible with Bootstrap 5.

---
+++ src/EbookArchiver.Web/gulpfile.js
@@ -0,0 +1,68 @@
+﻿"use strict";
+
+const gulp = require("gulp"),
+    babel = require('gulp-babel'),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-clean-css"),
+    uglify = require("gulp-uglify"),
+    merge = require("merge-stream"),
+    del = require("del"),
+    bundleconfig = require("./bundleconfig.json");
+
+const regex = {
+    css: /\.css$/,
+    js: /\.js$/
+};
+
+gulp.task("min:js", function () {
+    const tasks = getBundles(regex.js).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(concat(bundle.outputFileName))
+            .pipe(uglify())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:css", function () {
+    const tasks = getBundles(regex.css).map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(cssmin())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min", gulp.series("min:js", "min:css"));
+
+gulp.task("clean", function () {
+    const files = bundleconfig.map(function (bundle) {
+        return bundle.outputFileName;
+    });
+
+    return del(files);
+});
+
+gulp.task("watch", function () {
+    getBundles(regex.js).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:js"]);
+    });
+
+    getBundles(regex.css).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:css"]);
+    });
+
+    getBundles(regex.html).forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:html"]);
+    });
+});
+
+function getBundles(regexPattern) {
+    return bundleconfig.filter(function (bundle) {
+        return regexPattern.test(bundle.outputFileName);
+    });
+}
